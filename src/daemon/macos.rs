228337a421bf0573dc407b08d222ff267a8d271e
YuigaWada <yuigawada@gmail.com>
2020-10-30 22:23:40 +0900

    ログファイルを残すように

--- src/daemon/macos.rs
+++ src/daemon/macos.rs
@@ -2,7 +2,9 @@ use super::super::file;
 use std::path::PathBuf;
 use std::process::Command;
 
-const EXE_VARIABLE: &str = "{{ABSOLUTE_PATH}}";
+const EXE_VARIABLE: &str = "{{EXE_PATH}}";
+const LOG_VARIABLE: &str = "{{LOG_FILE_PATH}}";
+const ERROR_LOG_VARIABLE: &str = "{{ERROR_LOG_FILE_PATH}}";
 
 const PLIST_FILEPATH: &str = "~/Library/LaunchAgents/launch-port-snippet.plist";
 const PLIST_TEMPLATE: &str = r#"
@@ -14,31 +16,82 @@ const PLIST_TEMPLATE: &str = r#"
     <string>launch-port-snippet</string>
     <key>ProgramArguments</key>
     <array>
-        <string>{{ABSOLUTE_PATH}}</string>
+        <string>{{EXE_PATH}}</string>
         <string>AUTO_LAUNCH</string>
     </array>
     <key>RunAtLoad</key>
     <true/>
+    <key>StandardOutPath</key>
+    <string>{{LOG_FILE_PATH}}</string>
+    <key>StandardErrorPath</key>
+    <string>{{ERROR_LOG_FILE_PATH}}</string>
 </dict>
 </plist>
 "#;
 
-pub fn register() {
-    let home_dir = std::env::var("HOME").unwrap();
-    let exe_path = std::env::current_exe().expect("cannot get current_exe");
+struct LogPaths {
+    standard: PathBuf,
+    error: PathBuf,
+}
+
+// ログファイルのパスを取得する
+fn get_log_path(exe_path: &PathBuf) -> LogPaths {
+    let mut log_dir = exe_path.clone();
+    log_dir.pop();
+    log_dir.push(".log");
+
+    let mut log_path = log_dir.clone();
+    log_path.push("standard.log"); // > ./(exe)/.log/standard.log
+
+    let mut error_log_path = log_dir.clone();
+    error_log_path.push("error.log"); // > ./(exe)/.log/error.log
+
+    return LogPaths {
+        standard: log_path,
+        error: error_log_path,
+    };
+}
+
+// plistに変数を注入する
+fn inject_variables(mut plist: String, exe_path: PathBuf) -> String {
+    let log_paths = get_log_path(&exe_path);
     let exe_path_string = exe_path
         .into_os_string()
         .into_string()
         .expect("cannot get current_exe");
 
-    let mut plist = PLIST_TEMPLATE.to_string();
     plist = plist.replace(EXE_VARIABLE, &exe_path_string);
+    plist = plist.replace(
+        LOG_VARIABLE,
+        &log_paths
+            .standard
+            .to_str()
+            .expect("something went wrong: cannot convert PathBuf → str"),
+    );
+    plist = plist.replace(
+        ERROR_LOG_VARIABLE,
+        &log_paths
+            .error
+            .to_str()
+            .expect("something went wrong: cannot convert PathBuf → str"),
+    );
+
+    return plist;
+}
+
+// daemonを登録
+pub fn register() {
+    let home_dir = std::env::var("HOME").unwrap();
+    let exe_path = std::env::current_exe().expect("cannot get current_exe");
+
+    let mut plist = PLIST_TEMPLATE.to_string();
+    plist = inject_variables(plist, exe_path);
 
     let plist_filepath_string = PLIST_FILEPATH.replace("~", &home_dir);
     let plist_filepath = PathBuf::from(&plist_filepath_string);
     file::write_file(&plist_filepath, plist);
 
-    println!("> launchctl load {}",&plist_filepath_string);
+    println!("> launchctl load {}", &plist_filepath_string);
 
     // launchdで起動
     let launchctl = Command::new("launchctl")
