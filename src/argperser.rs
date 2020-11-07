acc5a1ccd97c2ccfb9363763965a88c2377dd251
YuigaWada <yuigawada@gmail.com>
2020-11-07 18:00:09 +0900

    argperser: 単体テスト追加

--- src/argperser.rs
+++ src/argperser.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum LaunchType {
     Man,     // 直接人間が起動させた場合
     Daemon,  // cron処理でpcが起動させた場合
@@ -85,3 +86,78 @@ fn get_params(short: bool) -> Params {
 
 // ヘルプを表示
 pub fn print_help() {}
+
+#[cfg(not(debug_assertions))]
+mod tests {
+    use crate::argperser::*;
+
+    // util
+
+    fn gen_mock_args(arg: &str) -> Vec<String> {
+        return vec!["MOCK_EXE_PATH".to_string(), arg.to_string()];
+    }
+
+    // shorts
+
+    #[test]
+    fn daemon_valid() {
+        let launch_type = detect_type(gen_mock_args("AUTO_LAUNCH"));
+        assert_eq!(launch_type, LaunchType::Daemon);
+    }
+
+    #[test]
+    fn short_daemon_invalid() {
+        let launch_type = detect_type(gen_mock_args("A"));
+        assert_ne!(launch_type, LaunchType::Daemon); // not!
+    }
+
+    #[test]
+    fn short_man_valid() {
+        let launch_type = detect_type(gen_mock_args("-m"));
+        assert_eq!(launch_type, LaunchType::Daemon);
+    }
+
+    #[test]
+    fn short_stop_valid() {
+        let launch_type = detect_type(gen_mock_args("-s"));
+        assert_eq!(launch_type, LaunchType::Stop);
+    }
+
+    #[test]
+    fn short_restart_valid() {
+        let launch_type = detect_type(gen_mock_args("-r"));
+        assert_eq!(launch_type, LaunchType::Restart);
+    }
+
+    #[test]
+    fn short_help_valid() {
+        let launch_type = detect_type(gen_mock_args("-h"));
+        assert_eq!(launch_type, LaunchType::Help);
+    }
+
+    // long
+
+    #[test]
+    fn long_man_valid() {
+        let launch_type = detect_type(gen_mock_args("man"));
+        assert_eq!(launch_type, LaunchType::Daemon);
+    }
+
+    #[test]
+    fn long_stop_valid() {
+        let launch_type = detect_type(gen_mock_args("stop"));
+        assert_eq!(launch_type, LaunchType::Stop);
+    }
+
+    #[test]
+    fn long_restart_valid() {
+        let launch_type = detect_type(gen_mock_args("restart"));
+        assert_eq!(launch_type, LaunchType::Restart);
+    }
+
+    #[test]
+    fn long_help_valid() {
+        let launch_type = detect_type(gen_mock_args("help"));
+        assert_eq!(launch_type, LaunchType::Help);
+    }
+}
